c43e0158a3f256d7a121058c77a3d42df248aa26
Syed Ali Fathima A <syedalifathima.a@publicissapient.com>
2022-11-08 21:06:19 +0530

    docker file added

--- src/main/java/com/example/jenkinsdockertwo/JenkinsDockerTwoApplication.java
+++ src/main/java/com/example/jenkinsdockertwo/JenkinsDockerTwoApplication.java
@@ -7,6 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JenkinsDockerTwoApplication {
 
 	public static void main(String[] args) {
+
+		System.out.println("Test two !!!");
+
 		SpringApplication.run(JenkinsDockerTwoApplication.class, args);
 	}
 
