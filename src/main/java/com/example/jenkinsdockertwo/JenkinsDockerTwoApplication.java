e571893e474fdff10096d9cbd1ae8c7414c02078
Syed Ali Fathima A <syedalifathima.a@publicissapient.com>
2022-11-08 20:01:32 +0530

    first commit

---
+++ src/main/java/com/example/jenkinsdockertwo/JenkinsDockerTwoApplication.java
@@ -0,0 +1,13 @@
+package com.example.jenkinsdockertwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsDockerTwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsDockerTwoApplication.class, args);
+	}
+
+}
